ed740e06dd9843d67d3581f45183a9b6be76a2cf
Filmon <FilmonK@gmail.com>
2020-07-16 21:53:49 -0500

    uploading jquery assignment

---
+++ week8/jquery/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#generateRandomDoge').click(function () {
+      console.log('button clicked');
+      $.get('https://dog.ceo/api/breeds/image/random')
+      .done(function (data) {
+        console.log(data);
+        $('#dogoContainer').html(renderDogoImage(data.message));
+      })
+      .fail(function (error) {
+        console.log(error);
+      });
+    });
+  
+    function renderDogoImage(image) {
+      return `
+      <img src="${image}" class="mw-100" />
+      `;
+    }
+  });
+
